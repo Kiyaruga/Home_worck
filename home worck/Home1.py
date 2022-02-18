9bf2169ad524697ec42875a56b91903303c100b0
Kiyariga <99130648+Kiyaruga@users.noreply.github.com>
2022-02-18 20:39:24 +0200

    +1 new wock

--- home worck/Home1.py
+++ home worck/Home1.py
@@ -1,17 +1,18 @@
-question = input('Фамилия автора:')
+name = input('Фамилия автора:')
 authors = {
     'Пушкин': 'Русский поэт, драматург и прозаик. Один из самых авторитетных литературных деятелей первой трети XIX века',
     'Толстой': 'Один из наиболее известных русских писателей и мыслителей, один из величайших писателей-романистов мира',
     'Бунин': 'Русский писатель, поэт и переводчик, лауреат Нобелевской премии по литературе 1933 года'}
-if question in authors:
-    print(authors[question])
-elif question not in authors:
-    answer2 = input('Автор не найлен.Дабавить?')
-    if answer2 == 'Да':
-        biography = input('Напишите билграфию')
-        print('Дабавил!')
-        authors[answer2] = biography
-        print(authors[answer2])
-        if answer2 != 'Нет':
-            print('Ответ получен')
+if name in authors:
+    print(authors[name])
+elif name not in authors:
+    question = input('Автор не найлен.Дабавить?:')
+    if question == 'Да':
+        biography = input('Напишите билграфию:')
+        print('Дабавил!',name)
+        authors[question] = biography
+    elif question == 'Нет':
+        print('Ответ получен')
+        
 print(authors)
+
